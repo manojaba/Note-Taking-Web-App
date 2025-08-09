1dd279f5df1e7fe3cda54f7937473d3c68d83f7a
manojaba <maanda.mannan@gmail.com>
2025-08-09 12:01:30 +0200

    tailwind plugin added in vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   base:'/Note-Taking-Web-App/',
   server:{
     port:5174,
